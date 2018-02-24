7b98ccd2aa864171578874aaea80508563122297
Roni Kettunen <ronikettunen96@gmail.com>
2018-02-24 15:15:05 +0200

    Exercise4.4 Done

--- exercise4/blogilista/utils/list_helper.js
+++ exercise4/blogilista/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  let amount = 0
+  for (let index in blogs) {
+    amount += blogs[index].likes
+  }
+  return amount
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
