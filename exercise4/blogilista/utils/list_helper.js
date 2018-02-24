a8698442b06ebdba85506f2ddfa873a16b0de250
Roni Kettunen <ronikettunen96@gmail.com>
2018-02-24 16:37:21 +0200

    Exercise4.5 and 4.6 Done

--- exercise4/blogilista/utils/list_helper.js
+++ exercise4/blogilista/utils/list_helper.js
@@ -10,7 +10,62 @@ const totalLikes = (blogs) => {
   return amount
 }
 
+const favoriteBlog = (blogs) => {
+  let biggest = -1
+  let favorite
+  for (let index in blogs) {
+    const temp = blogs[index].likes
+    if (biggest < temp) {
+      biggest = temp
+      favorite = blogs[index]
+    }
+  }
+  return favorite || 'no blogs to compare'
+}
+
+const mostBlogs = (blogs) => {
+  const collection = {
+    names: [],
+    numbers: []
+  }
+  let amount = 0
+  let most
+  for (let index in blogs) {
+    let contain = false
+    let place
+    const temp = blogs[index]
+    for (let i = 0; i < collection.names.length; i++) {
+      if (!contain) {
+        if (collection.names[i] === temp.author) {
+          contain = true
+          place = i
+        }
+      }
+    }
+    if (contain) {
+      collection.numbers[place]++
+    } else {
+      collection.names.push(temp.author)
+      collection.numbers.push(1)
+    }
+  }
+
+  const highestnumberindex = collection.numbers.indexOf(Math.max(...collection.numbers))
+  if (highestnumberindex !== -1) {
+    return (
+      {
+        author: collection.names[highestnumberindex],
+        blogs: collection.numbers[highestnumberindex]
+      }
+    )
+  } else {
+    return 'no blogs'
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog,
+  mostBlogs
 }
