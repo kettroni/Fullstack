7ecfaf7e752d5996d4d4f7c0f32ae31e1efd776f
Roni Kettunen <ronikettunen96@gmail.com>
2018-02-24 12:26:59 +0200

    Exercise4.2 Done

--- exercise4/blogilista/index.js
+++ exercise4/blogilista/index.js
@@ -3,43 +3,26 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
-require('dotenv').config()
 
-const Blog = mongoose.model('Blog', {
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-module.exports = Blog
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then( () => {
+    console.log('connected to database')
+  })
+  .catch( err => console.log(err))
 
 app.use(cors())
 app.use(bodyParser.json())
+app.use(middleware.logger)
 
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
+app.use('/api/blogs', blogsRouter)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+app.use(middleware.error)
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
+const PORT = process.env.PORT || 3003
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
