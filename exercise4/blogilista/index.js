4390a5d77401932479528d6a2c30cd9574639826
Roni Kettunen <ronikettunen96@gmail.com>
2018-02-25 20:53:19 +0200

    Exercise4.20 Done

--- exercise4/blogilista/index.js
+++ exercise4/blogilista/index.js
@@ -3,9 +3,9 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
-const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const loginRouter = require('./controllers/login')
@@ -21,13 +21,14 @@ mongoose
 app.use(cors())
 app.use(bodyParser.json())
 app.use(middleware.logger)
+app.use(middleware.tokenExtractor)
+
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 
 app.use(middleware.error)
-
 const server = http.createServer(app)
 
 const PORT = config.port
--- exercise4/blogilista/utils/middleware.js
+++ exercise4/blogilista/utils/middleware.js
@@ -13,7 +13,19 @@ const error = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    request.token = authorization.substring(7)
+    next()
+  } else {
+    request.token = null
+    next()
+  }
+}
+
 module.exports = {
   logger,
-  error
+  error,
+  tokenExtractor
 }
