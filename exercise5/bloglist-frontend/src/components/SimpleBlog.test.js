c360a4269b3d84daa2240035fc8116f71496e8a9
Roni Kettunen <ronikettunen96@gmail.com>
2018-03-04 20:57:16 +0200

    Exercise5.12 Done

---
+++ exercise5/bloglist-frontend/src/components/SimpleBlog.test.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import SimpleBlog from './SimpleBlog'
+
+describe.only('<SimpleBlog />', () => {
+
+  const temp = {
+    title: 'Komponenttitestaus tapahtuu jestillä ja enzymellä',
+    author: 'FIksu',
+    url: 'www.asdasdasd.com',
+    likes: 0
+  }
+
+  const blogComponent = shallow(<SimpleBlog blog={temp} />)
+
+
+  it('renders title and author', () => {
+    const contentDiv = blogComponent.find('.head')
+    expect(contentDiv.text()).toContain(temp.title && temp.author)
+  })
+
+  it('renders amount of likes', () => {
+    const contentDiv = blogComponent.find('.likes')
+    expect(contentDiv.text()).toContain(temp.likes)
+  })
+})
