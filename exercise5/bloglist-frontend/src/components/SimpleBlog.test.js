61415998fc4604315b2e1679d0ad97ccdc21ae4c
Roni Kettunen <ronikettunen96@gmail.com>
2018-03-04 21:08:00 +0200

    Exercise5.13 Done

--- exercise5/bloglist-frontend/src/components/SimpleBlog.test.js
+++ exercise5/bloglist-frontend/src/components/SimpleBlog.test.js
@@ -15,12 +15,34 @@ describe.only('<SimpleBlog />', () => {
 
 
   it('renders title and author', () => {
+
     const contentDiv = blogComponent.find('.head')
+
     expect(contentDiv.text()).toContain(temp.title && temp.author)
   })
 
   it('renders amount of likes', () => {
+
     const contentDiv = blogComponent.find('.likes')
+
     expect(contentDiv.text()).toContain(temp.likes)
+  })
+
+  it('clicking the like button twice calls event handler twice', () => {
+
+    const mockHandler = jest.fn()
+
+    const noteComponent = shallow(
+      <SimpleBlog
+        blog={temp}
+        onClick={mockHandler}
+      />
+    )
+
+    const button = noteComponent.find('button')
+    button.simulate('click')
+    button.simulate('click')
+
+    expect(mockHandler.mock.calls.length).toBe(2)
   })
 })
