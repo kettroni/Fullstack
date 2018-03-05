a8b61a3c9b7f50b7f00b4dc94ba0bfd532484e87
Roni Kettunen <ronikettunen96@gmail.com>
2018-03-05 20:12:40 +0200

    Fixed Exercise6.13

--- exercise6/redux-anecdotes-v2/src/App.js
+++ exercise6/redux-anecdotes-v2/src/App.js
@@ -2,14 +2,12 @@ import React from 'react'
 import Notification from './components/Notification'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
-import AnecdoteService from './services/anecdotes'
 import { anecInitialization } from './reducers/anecdoteReducer'
 import { connect } from 'react-redux'
 
 class App extends React.Component {
-  componentDidMount = async () => {
-    const anecdotes = await AnecdoteService.getAll()
-    this.props.anecInitialization(anecdotes)
+  componentDidMount() {
+    this.props.anecInitialization()
   }
   render() {
     return (
